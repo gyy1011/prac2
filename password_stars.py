1de61dd564db51794dde8234386d75dfac6ee81f
gao yangyu <1005654364@qq.com>
2022-11-18 17:14:18 +0800

    password star

--- password_stars.py
+++ password_stars.py
@@ -5,22 +5,23 @@ MINIMUM_LENGTH = 4
 
 
 def version_1():
+    """get a password"""
     password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
     while len(password) < MINIMUM_LENGTH:
         password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
 
     print('*' * len(password))
 
-
-# version_1()
-
+# version1
 
 def main():
+    """use function"""
     password = get_password(MINIMUM_LENGTH)
     print_asterisks(password)
 
 
 def get_password(minimum_length):
+    """get the requirement"""
     password = input(f"Enter password of at least {minimum_length} characters: ")
     while len(password) < minimum_length:
         print("Password too short")
@@ -29,6 +30,7 @@ def get_password(minimum_length):
 
 
 def print_asterisks(sequence):
+    """print the sequence."""
     print('*' * len(sequence))
 
 
