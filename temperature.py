434cce6ef94b2cb5e1a635389e00253e7d1eec7e
gao yangyu <1005654364@qq.com>
2022-11-18 17:47:17 +0800

    temperature conversion

---
+++ temperature.py
@@ -0,0 +1,37 @@
+"""set and define the menu"""
+MENU = """
+F - Convert Fahrenheit to Celsius
+C - Convert Celsius to Fahrenheit
+Q - Quit"""
+
+
+def main():
+    """Temperature conversion program."""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "F":
+            fahrenheit = float(input("Fahrenheit : "))
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        elif choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """two different conversion"""
+    return 5 / 9 * (fahrenheit - 32)
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+main()
