aa699a85487d2110db1f09e8cdc58966bfcd7f1e
gao yangyu <1005654364@qq.com>
2022-11-18 17:20:31 +0800

    score result

--- score.py
+++ score.py
@@ -1,9 +1,11 @@
 def main():
+    """get a score and input."""
     score = float(input("Enter score: "))
     print(determine_status(score))
 
 
 def determine_status(score):
+    """different score and get different result"""
     if score < 0 or score > 100:
         return "Invalid score"
     elif score >= 90:
